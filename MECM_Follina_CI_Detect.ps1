7d347c880f2b8ef59dea61c2ea452e336c7e46ed
Julian West <west.julian@me.com>
2022-06-01 00:54:28 -0500

    Update MECM_Follina_CI_Detect.ps1

--- MECM_Follina_CI_Detect.ps1
+++ MECM_Follina_CI_Detect.ps1
@@ -1,3 +1,5 @@
+# See this for more info: https://www.cisa.gov/uscert/ncas/current-activity/2022/05/31/microsoft-releases-workaround-guidance-msdt-follina-vulnerability
+#
 # checks for HKCR\ms-msdt key and reports true/false back to MECM Configuration Item Script
 # see MECM_Follina_CI_Detect.ps1 for the remediation action
 
