b87d3fc57842c94b66fab63c327b82a49c91bfbc
Julian West <west.julian@me.com>
2022-06-01 00:54:59 -0500

    Update MECM_Follina_CI_Remediate.ps1

--- MECM_Follina_CI_Remediate.ps1
+++ MECM_Follina_CI_Remediate.ps1
@@ -1,3 +1,5 @@
-# If MECM_Follina_CI_Detect.ps1 returns bool $true during CI/CB pass, this Remediation action nukes the ms-msdt key
+# See this for more info: https://www.cisa.gov/uscert/ncas/current-activity/2022/05/31/microsoft-releases-workaround-guidance-msdt-follina-vulnerability
+#
+# If MECM_Follina_CI_Detect.ps1 returns bool $true during an MECM CI/CB pass, this Remediation action nukes the ms-msdt key
 
 Remove-Item -Path "Registry::HKEY_CLASSES_ROOT\ms-msdt" -Recurse -Force -ErrorAction SilentlyContinue
