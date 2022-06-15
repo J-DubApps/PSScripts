15ec904b5b5592ea528baae5bf70e75629e42c4d
Julian West <west.julian@me.com>
2022-06-15 09:55:13 -0500

    Update Test-AzureCloudShell.ps1

--- Test-AzureCloudShell.ps1
+++ Test-AzureCloudShell.ps1
@@ -1,4 +1,5 @@
-  <#
+#Requires -Version 5.1
+<#
                Name: Test-AzureCloudShell.ps1
                31 May 2022
            .NOTES
@@ -34,17 +35,17 @@
    :-------------------------------------------------------------------------:
    `---._.-------------------------------------------------------------._.---'
            .SYNOPSIS
-               Check if this PS Session is operating within a remote Azure Cloud Shell 
+                Check if this PS Session is operating within a remote Azure Cloud Shell 
            .DESCRIPTION
-               Function to check if the current PS Session is operating within a remote 
+               Simple Function to check if the current PS Session is operating within a remote 
                Azure Cloud Shell.  It will return $true / $false depending on the PS
-               environment.  Use this Function alone or with a variable of your choice.
+               environment.
                
                NOTE: Depends on Microsoft to continue setting the "OS" entry within 
                $PSVersionTable to contain the word "azure".  If they change that down the line,
                just review & revise the string-selection pattern in the function or use RegEx.
                
-               USAGE: No parameters required, Just call the function!
+               USAGE: No parameters required, Just call the function.
            .EXAMPLE
                # Assign the function to a variable that gets set to $true or $false if the PS
                # session is running within a remote Azure Cloud Shell
@@ -52,16 +53,41 @@
                	$IsACS = Test-AzureCloudShell
        #>
 
-Function Test-AzureCloudShell {
+       Function Test-AzureCloudShell {
 
-    $ACScheck = $PSVersionTable | select-object OS | select-string -pattern "azure" | Out-String
-
-	If(($PSVersionTable.PSEdition.ToString() -eq 'Core') -and ($ACScheck -match 'azure')){
-	# Test $PSVersionTable PSEdition & OS outputs for two tell-tale Azure Cloud environment signs
-	# Return $true if both output conditions are present, $false if no match.
-	
-		return $true
-    }else{
-    	return $false
+        $ACScheck = $PSVersionTable | select-object OS | select-string -pattern "azure" | Out-String
+    
+        If(($PSVersionTable.PSEdition.ToString() -eq 'Core') -and ($ACScheck -match 'azure')){
+        # Test $PSVersionTable PSEdition & OS outputs for two tell-tale Azure Cloud environment signs
+        # Return $true if both output conditions are present, $false if no match.
+        
+            return $true
+        }else{
+            return $false
+        }
     }
-}
+    
+    #region LICENSE
+    <#
+          Creative GNU General Public License, version 3 (GPLv3)
+          Julian West
+          Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+          1. Redistributions of source code must retain the above LICENSE information, this list of conditions and the following disclaimer.
+          2. Redistributions in binary form must reproduce the above LICENSE notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+          3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+          THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+    #>
+    #endregion LICENSE
+    
+    #region DISCLAIMER
+    <#
+          DISCLAIMER:
+          - Use at your own risk, etc.
+          - This is open-source software, if you find an issue try to fix it yourself. There is no support and/or warranty in any kind
+          - This is a third-party Software
+          - The developer of this Software is NOT sponsored by or affiliated with Microsoft Corp (MSFT) or any of its subsidiaries in any way
+          - The Software is not supported by Microsoft Corp (MSFT)
+          - By using the Software, you agree to the License, Terms, and any Conditions declared and described above
+          - If you disagree with any of the terms, and any conditions declared: delete it and build your own solution
+    #>
+    #endregion DISCLAIMER
