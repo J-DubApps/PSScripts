b3c81e85e0cbe63ea53453d4d40308864318a770
Julian West <west.julian@me.com>
2023-09-21 12:55:30 -0500

    Update get-AutoreplyUsers.ps1

--- get-AutoreplyUsers.ps1
+++ get-AutoreplyUsers.ps1
@@ -140,7 +140,7 @@ exit 0
 <#
       BSD 3-Clause License
 
-      Copyright (c) 2022, Julian West
+      Copyright (c) 2023, Julian West
       All rights reserved.
 
       Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
